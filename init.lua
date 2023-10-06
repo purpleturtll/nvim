5db4c5e6013f44836fe4c2e9cff6bac35b03d1d6
Bartek Ciesielczyk <purpleturtll@gmail.com>
2023-10-06 16:11:09 +0200

    add fuzzy-finder

--- init.lua
+++ init.lua
@@ -16,8 +16,21 @@ end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
     "tpope/vim-sensible",
-    "tpope/vim-surround"
+    "tpope/vim-surround",
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim"
 })
+
+-- Remaps
+-- ------
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
 -- The rest
 -- --------
+
+vim.api.nvim_command("Ntree")
